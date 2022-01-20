b7790c8aa51cba89af0706f4043b2c70ed622df0
Pascal Hertleif <pascal@technocreatives.com>
2022-01-20 11:31:11 +0100

    Silence babel errors

--- babel.config.js
+++ babel.config.js
@@ -11,6 +11,18 @@ module.exports = (api) => {
                 }
             ],
             [
+                '@babel/plugin-proposal-private-property-in-object',
+                {
+                    loose: true
+                }
+            ],
+            [
+                '@babel/plugin-proposal-private-methods',
+                {
+                    loose: true
+                }
+            ],
+            [
                 '@babel/plugin-transform-runtime',
                 {
                     corejs: 3
